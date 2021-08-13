3c1a0782ec81d343094792a1d5fe78a7130b2063
Eli Flores <eli@mignonnesaurus.com>
2021-08-13 20:25:32 +0200

    Improvements to start-miklusters.sh (#1)
    * Before we start the minikube cluster, we want to check that docker is running 🐳
    * Add better messaging here and there ✏️

--- scripts/start-mikluster.sh
+++ scripts/start-mikluster.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+start_minikube_cluster(){
+    KLUSTER_PROFILE_NAME=$1
+
+    echo "Going to start minikube cluster with profile name: ${KLUSTER_PROFILE_NAME} 🌱"
+
+    minikube start --memory=8192 --cpus=3 --kubernetes-version=v1.21.3 --vm-driver=virtualbox -p "${KLUSTER_PROFILE_NAME}"
+
+    CLUSTER_STARTED=$?
+
+    if [[ $CLUSTER_STARTED -eq 0 ]]; then 
+        echo "The minikube cluster ${KLUSTER_PROFILE_NAME} is up and running 🎉 and it's status is:"
+        echo "minikube status -p ${KLUSTER_PROFILE_NAME}"
+        minikube status -p "${KLUSTER_PROFILE_NAME}"
+    fi
+}
+
+check_docker_is_running_and_wait(){
+    IS_DOCKER_RUNNING=$(docker stats --no-stream 2>/dev/null)
+
+    if [[ ! ${IS_DOCKER_RUNNING} ]]; then
+        echo "Docker is not running, so we will start it 🐳"
+        open /Applications/Docker.app/
+    fi 
+
+    while [[ ! ${IS_DOCKER_RUNNING} ]]; do
+        IS_DOCKER_RUNNING=$(docker stats --no-stream 2>/dev/null)
+        sleep 2
+    done
+}
+
+# Check that a name for the cluster profile was provided
 if [[ $# -ne 1 ]]; then 
     echo "Usage: $0 cluster-profile-name"
     exit 1
 fi 
 
-KLUSTER_PROFILE_NAME=$1
-MIKLUSTER_START_EC=$(minikube start --memory=8192 --cpus=3 --kubernetes-version=v1.21.3 --vm-driver=virtualbox -p "${KLUSTER_PROFILE_NAME}")
-exit "${MIKLUSTER_START_EC}"
+# Having the Docker daemon running is a prerequisite to start a minikube cluster
+# So, first we check that the Docker is running and if it's not, we wait until it is running.
+check_docker_is_running_and_wait
+
+echo "Docker is running, now we'll start the minikube cluster ⛴"
+start_minikube_cluster "${1}"
