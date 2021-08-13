94ef5eb2daa7bf2d11951060448d345f3e56b186
Eli Flores <eli@mignonnesaurus.com>
2021-08-13 17:00:45 +0000

    Fix files

--- scripts/start-mikluster.sh
+++ scripts/start-mikluster.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
+start_minikube_cluster(){
+    KLUSTER_PROFILE_NAME=$1
+
+    echo "Going to start minikube cluster with profile name ${KLUSTER_PROFILE_NAME} 🌱"
+
+    minikube start --memory=8192 --cpus=3 --kubernetes-version=v1.21.3 --vm-driver=virtualbox -p "${KLUSTER_PROFILE_NAME}"
+
+    CLUSTER_STARTED=$?
+
+    if [[ $CLUSTER_STARTED -eq 0 ]]; then 
+        echo "The minikube cluster ${KLUSTER_PROFILE_NAME} is started 🎉"
+    fi
+}
+
+check_docker_is_running_and_wait(){
+    IS_DOCKER_RUNNING=$(docker stats --no-stream)
+    while [[ ! ${IS_DOCKER_RUNNING} ]]; do
+        open /Applications/Docker.app/
+        sleep 2
+        IS_DOCKER_RUNNING=$(docker stats --no-stream 2>/dev/null)
+    done
+}
+
+# Check that a name for the cluster profile was provided
 if [[ $# -ne 1 ]]; then 
     echo "Usage: $0 cluster-profile-name"
     exit 1
 fi 
 
-KLUSTER_PROFILE_NAME=$1
-MIKLUSTER_START_EC=$(minikube start --memory=8192 --cpus=3 --kubernetes-version=v1.21.3 --vm-driver=virtualbox -p "${KLUSTER_PROFILE_NAME}")
-exit "${MIKLUSTER_START_EC}"
+# Having the Docker daemon running is a prerequisite to start a minikube cluster
+# So, first we check that the Docker is running and if it's not, we wait until it is running.
+check_docker_is_running_and_wait
+
+start_minikube_cluster "${1}"
