d98577632a3f5439c323c8e519253daf8c496481
eliflores <eli@mignonnesaurus.com>
2021-08-27 21:20:16 +0200

    Fix up Oh My Zsh plugins test

--- test/setup-macos-test.sh
+++ test/setup-macos-test.sh
@@ -1,40 +1,30 @@
 #!/usr/bin/env bats
 
 @test 'Homebrew is installed' {
-  run brew --version
-  (( status == 0 ))
+  brew --version
 }
 
 @test 'Git is installed and setup' {
-  run git --version
-  (( status == 0 ))
-
-  run [ -f "${HOME}"/.gitconfig ]
-  (( status == 0 ))
+  git --version
+  [[ -f "${HOME}"/.gitconfig ]]
 }
 
 @test 'Oh My Zsh is setup' {
-  run [ -f "${HOME}"/.zshrc ]
-  (( status == 0 ))
+  [[ -f "${HOME}"/.zshrc ]]
 }
 
 @test 'Oh My Zsh theme is setup' {
-  run grep 'ZSH_THEME=' "${HOME}"/.zshrc
-  (( status == 0 ))
-  [[ "${output}" = "ZSH_THEME=\"awesomepanda\"" ]]
+  INSTALLED_THEME=$(grep 'ZSH_THEME=' "${HOME}"/.zshrc | sed -n 1p | sed 's/^[[:space:]]*//')
+  [[ "${INSTALLED_THEME}" = "ZSH_THEME=\"awesomepanda\"" ]]
 }
 
 @test 'Oh My Zsh plugins are setup' {
-  run grep 'plugins=' "${HOME}"/.zshrc | sed -n 2p
-  (( status == 0 ))
+  grep 'plugins=' "${HOME}"/.zshrc | sed -n 2p | sed 's/^[[:space:]]*//'
   [[ "${output}" = "plugins=(git dotenv osx python kubectl docker zsh-completions zsh-syntax-highlighting zsh-autosuggestions)" ]]
 
-  run [ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-completions ]
-  (( status == 0 ))
+  [[ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-completions ]]
 
-  run [ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]
-  (( status == 0 ))
+  [[ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]
 
-  run [ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]
-  (( status == 0 ))
+  [[ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]
 }
