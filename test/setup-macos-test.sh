e73e89342a401c4d7e15c8a8a243e04ce4d6c2ed
eliflores <eli@mignonnesaurus.com>
2021-08-27 17:07:28 +0200

    Add tests

--- test/setup-macos-test.sh
+++ test/setup-macos-test.sh
@@ -1,10 +1,33 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bats
 
-# local version: 1.1.0.0
+@test 'Homebrew is installed' {
+  run brew --version
+  (( status == 0 ))
+}
 
-@test "Setup macOS!" {
-  run bash ../scripts/setup-macos-test.sh
-
+@test 'Git is installed and setup' {
+  run git --version
   (( status == 0 ))
 
+  run [ -f "${HOME}"/.gitconfig ]
+  (( status == 0 ))
+}
+
+@test 'Oh my Zsh is setup' {
+  run [ -f "${HOME}"/.zshrc ]
+  (( status == 0 ))
+
+  run grep 'ZSH_THEME=' "${HOME}"/.zshrc | sed -n 1p
+  (( status == 0 ))
+  [[ "${output}" = "ZSH_THEME=\"awesomepanda\"" ]]
+
+  run grep 'plugins=' "${HOME}"/.zshrc | sed -n 2p
+  (( status == 0 ))
+  [[ "${output}" = "plugins=(git dotenv osx python zsh-syntax-highlighting zsh-autosuggestions kubectl docker)" ]]
+
+  run [ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]
+  (( status == 0 ))
+
+  run [ -d "${HOME}"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]
+  (( status == 0 ))
 }
