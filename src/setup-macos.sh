c47c28434661487211f19432cd299281934c2c7f
Eli Flores <eli@mignonnesaurus.com>
2021-08-30 16:21:06 +0000

    Call install Docker tools function
    This will install Docker Tools.

--- src/setup-macos.sh
+++ src/setup-macos.sh
@@ -89,15 +89,6 @@ setup_git() {
     cat "${HOME}/"/.gitconfig
 }
 
-install_kubernetes_tools(){
-    KUBERNETES_TOOLS=(stern kubernetes-cli minikube)
-    echo "Installing Kubernetes tools: ${KUBERNETES_TOOLS[*]}"
-    
-    for TOOL in "${KUBERNETES_TOOLS[@]}"; do 
-        brew install "${TOOL}"
-    done
-}
-
 install_docker_tools(){
     DOCKER_TOOLS=(dive)
     echo "Installing Docker tools"
@@ -105,6 +96,16 @@ install_docker_tools(){
     for TOOL in "${DOCKER_TOOLS[@]}"; do 
         brew install "${TOOL}"
     done 
+}
+
+
+install_kubernetes_tools(){
+    KUBERNETES_TOOLS=(stern kubernetes-cli minikube)
+    echo "Installing Kubernetes tools: ${KUBERNETES_TOOLS[*]}"
+    
+    for TOOL in "${KUBERNETES_TOOLS[@]}"; do 
+        brew install "${TOOL}"
+    done
 }
 
 echo "Going to setup a new macOS 💻"
@@ -118,6 +119,7 @@ install_ohmyzsh_plugins
 setup_ohmyzsh_theme
 setup_ohmyzsh_plugins
 setup_git
+install_docker_tools
 
 echo "All done! ✅"
 exit 0
