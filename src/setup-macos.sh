e4f3f3b4b04072131c117e67276e5f209fc54960
eliflores <eli@mignonnesaurus.com>
2023-05-31 13:17:24 +0200

    Add the possibility to run the script directly
    Without needing to clone the whole repository.

--- src/setup-macos.sh
+++ src/setup-macos.sh
@@ -3,6 +3,8 @@
 # This script installs some of the tools that I most generally use to work and play, on a macOS host 🍎
 # It requires Homebrew to already be installed on the host where it will run.
 
+# $ /bin/bash -c "$(curl -fSL https://github.com/patitalabs/bashecitos/blob/VERSION/src/setup-macos.sh)"
+
 set -e
 
 function print_preface() {
@@ -99,7 +101,9 @@ function setup_git() {
     if [[ $(echo "${SETUP_GIT}" | tr '[:upper:]' '[:lower:]') == 'y' ]]; then
         echo "Setting up Git 🐱"
 
-        sed -e "s/REPLACE_ME_WITH_GITHUB_USERNAME/${GITHUB_USERNAME}/" src/templates/gitconfig | sed -e "s/REPLACE_ME_WITH_GITHUB_EMAIL/${GITHUB_EMAIL}/" >"${HOME}"/.gitconfig
+        curl -fsSL https://raw.githubusercontent.com/patitalabs/bashecitos/main/src/templates/gitconfig -o "${HOME}"/.gitconfig
+
+        sed -e "s/REPLACE_ME_WITH_GITHUB_USERNAME/${GITHUB_USERNAME}/" "${HOME}"/.gitconfig | sed -e "s/REPLACE_ME_WITH_GITHUB_EMAIL/${GITHUB_EMAIL}/" "${HOME}"/.gitconfig
 
         echo "Git was setup with the username: ${GITHUB_USERNAME} and email: ${GITHUB_EMAIL}."
         echo "The ${HOME}/.gitconfig file is the following: "
